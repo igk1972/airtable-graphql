dd35b5b146649f93b98ff2f9811f368d3611b009
Jeremy Moseley <jeremy@jeremymoseley.net>
2019-08-19 17:08:26 -0700

    Remove apollo server dep.

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
-const { printSchema } = require("graphql");
-const { ApolloServer } = require("apollo-server");
-const convertSchema = require("./convertSchema");
-const createResolvers = require("./createResolvers");
-const airtable = require("airtable");
-const fs = require('fs')
+const convertSchema = require('./convertSchema');
+const createResolvers = require('./createResolvers');
+const airtable = require('airtable');
+const fs = require('fs');
 
 class AirtableGraphQL {
   constructor(apiKey, config = {}) {
     this.columns = {};
     airtable.configure({ apiKey });
-    const schema = JSON.parse(fs.readFileSync(config.schemaPath || "./schema.json", "utf8"));
+    const schema = JSON.parse(
+      fs.readFileSync(config.schemaPath || './schema.json', 'utf8'),
+    );
 
-    var normalizedPath = require("path").join(__dirname, "columns");
-    require("fs")
+    var normalizedPath = require('path').join(__dirname, 'columns');
+    require('fs')
       .readdirSync(normalizedPath)
       .forEach(file => {
-        require("./columns/" + file)(this);
+        require('./columns/' + file)(this);
       });
 
     this.api = airtable.base(schema.id);
     this.schema = convertSchema(schema, this.columns);
 
-    this.resolvers = createResolvers(
-      schema,
-      this.api,
-      this.columns
-    );
-
-    this.server = new ApolloServer({
-      typeDefs: printSchema(this.schema),
-      resolvers: this.resolvers,
-      playground: config.playground,
-      introspection: true
-    });
+    this.resolvers = createResolvers(schema, this.api, this.columns);
   }
 
   addColumnSupport(columnType, config) {
     this.columns = {
       ...this.columns,
-      [columnType]: config
+      [columnType]: config,
     };
-  }
-
-  async listen(options) {
-    this.server.listen(options).then(({ url }) => {
-      console.log(`🚀  Server ready at ${url}`);
-    });
   }
 }
 
